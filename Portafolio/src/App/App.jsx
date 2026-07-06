8995d62b632d62454d20ad9e712bb15303f490d1
aalvarez-2024004 <aalvarez-2024004@kinal.edu.gt>
2026-07-05 23:28:55 -0600

    Se agrego la seccion de sobre mi

--- Portafolio/src/App/App.jsx
+++ Portafolio/src/App/App.jsx
@@ -1,9 +1,11 @@
 import Bienvenida from "../components/Bienvenida";
+import SobreMi from "../components/SobreMi";
 
 function App() {
   return (
     <>
       <Bienvenida />
+      <SobreMi />
     </>
   );
 }
---
+++ Portafolio/src/components/SobreMi.jsx
@@ -0,0 +1,26 @@
+import "../styles/SobreMi.css";
+
+function SobreMi() {
+  return (
+    <section className="sobremi">
+
+      <h2 className="sobremi-titulo">
+        SOBRE MÍ.
+      </h2>
+
+      <p className="sobremi-descripcion">
+        Soy estudiante de último año para graduarme como{" "}
+        <span className="highlight">Perito en Informática</span>, con la
+        meta de continuar mi formación en{" "}
+        <span className="highlight">Ingeniería en Sistemas</span>. Me
+        considero una persona responsable y apasionada por la{" "}
+        <span className="highlight">tecnología</span>; disfruto{" "}
+        <span className="highlight">programar</span>,{" "}
+        <span className="highlight">resolver problemas</span> y diseñar
+        páginas web con mucha <span className="highlight">creatividad</span>.
+      </p>
+    </section>
+  );
+}
+
+export default SobreMi;
