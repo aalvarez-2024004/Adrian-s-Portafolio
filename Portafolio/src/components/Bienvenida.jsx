e7f061216382feecfa3299144d532fb84c43721e
ftaalvarez-2024004 <ftaalvarez-2024004@kinal.edu.gt>
2026-07-09 10:56:22 -0600

    Se quito una g que no permitia reproducir el video en la bienvenida

--- Portafolio/src/components/Bienvenida.jsx
+++ Portafolio/src/components/Bienvenida.jsx
@@ -11,7 +11,7 @@ function Bienvenida() {
         src={videoFondo}
         autoPlay
         loop
-        mutedg
+        muted
         playsInline
       />
       <div className="hero-overlay" />
